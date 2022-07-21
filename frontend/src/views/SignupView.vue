f81a26ddc74af0c46ce7b5fa75211bbc6f01a43d
YoungJun <2753dudwns@naver.com>
2022-07-21 17:56:13 +0900

    isStudent Selectform Design

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -27,22 +27,25 @@
         <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
         <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호 확인</label>
       </div>
-      <ul class="grid gap-6 w-3/5 mr-auto ml-auto md:grid-cols-2">
-        <li>
-          <input type="radio" id="student" name="role" value="student" class="hidden peer" required checked>
-          <label for="student" class="flex items-center p-2 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <div class="text-lg mr-auto ml-auto text-center font-semibold">수강생</div>
-          </label>
-        </li>
-        <li>
-          <input type="radio" id="lecturer" name="role" value="lecturer" class="hidden peer">
-          <label for="lecturer" class="flex items-center p-2 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <div class="text-lg mr-auto ml-auto text-center font-semibold">강의자</div>
-          </label>
-        </li>
-      </ul>
+      <div class="flex justify-between items-center w-3/4 mr-auto ml-auto mb-6">
+        <div class="text-sm text-gray-500 dark:text-gray-400">분류</div>
+        <div class="flex justify-between">
+          <div class="mr-4">
+            <input type="radio" id="student" name="role" value="student" class="hidden peer" required checked>
+            <label for="student" class="flex items-center py-2 px-10 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-[#6c9cc6] peer-checked:text-white peer-checked:bg-[#6c9cc6] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <div class="text-sm mr-auto ml-auto text-center font-semibold">수강생</div>
+            </label>
+          </div>
+          <div>
+            <input type="radio" id="lecturer" name="role" value="lecturer" class="hidden peer">
+            <label for="lecturer" class="flex items-center py-2 px-10 w-full text-gray-500 rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:text-white dark:peer-checked:bg-[#666666] peer-checked:border-white peer-checked:border-[3px] dark:peer-checked:border-[3px] peer-checked:text-[#2c5172] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-[#353535]">
+              <div class="text-sm mr-auto ml-auto text-center font-semibold">강의자</div>
+            </label>
+          </div>
+        </div>
+      </div>
       <div class="text-center w-3/4 mr-auto ml-auto mb-6">
-        <button type="submit" class="text-white font-bold bg-[#2c5172] focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">회원가입</button>
+        <button type="submit" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">회원가입</button>
       </div>
     </form>
   </div>
