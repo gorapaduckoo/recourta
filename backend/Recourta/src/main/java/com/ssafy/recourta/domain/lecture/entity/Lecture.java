9400809392efa15c7e65737328c3a71bbc29f0e9
이지영 <jioung93@naver.com>
2022-07-24 23:57:44 +0900

    ..

--- backend/Recourta/src/main/java/com/ssafy/recourta/domain/lecture/entity/Lecture.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/lecture/entity/Lecture.java
@@ -1,9 +1,29 @@
-package com.ssafy.recourta.domain.lecture;
+package com.ssafy.recourta.domain.lecture.entity;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Lecture {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int lectureId;
     private String title;
     private String desc;
     private Date startDate;
+    private Date endDate;
+    private String lectureImg;
+
 }
