16db36113ee9fae2e05a1c341a0479aa866adf83
이지영 <jioung93@naver.com>
2022-07-24 16:09:12 +0900

    ..

---
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/lecture/entity/Lecture.java
@@ -0,0 +1,9 @@
+package com.ssafy.recourta.domain.lecture;
+
+public class Lecture {
+
+    private int lectureId;
+    private String title;
+    private String desc;
+    private Date startDate;
+}
