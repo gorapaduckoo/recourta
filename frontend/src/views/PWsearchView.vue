da036b48ba31edb78174e56a9a308c79da49391c
jiwan <jiwan1121@naver.com>
2022-07-25 14:05:00 +0900

    SignUpView Form Complete

--- frontend/src/views/PWsearchView.vue
+++ frontend/src/views/PWsearchView.vue
@@ -20,38 +20,41 @@
         <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
         <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
       </div>
+      <!-- modal button -->
       <div class="mt-16 text-center w-3/4 mr-auto ml-auto mb-6">
         <button data-modal-toggle="EmailModal" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">이메일 발송</button>
-      </div>
-
-    </form>
-  </div>
-    <!-- Main modal -->
-    <div id="EmailModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden ml-auto mr-auto fixed z-50 w-1/2 md:inset-0 h-modal md:h-full">
-      <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
-          <div class="flex justify-between items-start rounded-t dark:border-gray-600">
-            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="EmailModal">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-          </div>
-          <!-- Modal body -->
-          <div class="py-4 text-center">
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                비밀번호 재설정 메일을 보내시겠습니까?
-              </p>
-          </div>
-          <!-- Modal footer -->
-          <div class="flex items-center justify-center p-6 space-x-24 rounded-b border-t border-gray-200 dark:border-gray-600">
-              <button data-modal-toggle="EmailModal" type="button" class="text-white bg-[#2c5172] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#2c5172] dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-              <button data-modal-toggle="EmailModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+      </div>  
+      <!-- Main modal -->
+      <div id="EmailModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden ml-auto mr-auto fixed z-50 w-1/2 md:inset-0 h-modal md:h-full">
+        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+          <!-- Modal content -->
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex justify-between items-start rounded-t dark:border-gray-600">
+              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="EmailModal">
+                  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  <span class="sr-only">Close modal</span>
+              </button>
+            </div>
+            <!-- Modal body -->
+            <div class="py-4 text-center">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  비밀번호 재설정 메일을 보내시겠습니까?
+                </p>
+            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center justify-center p-6 space-x-24 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button data-modal-toggle="EmailModal" type="button" class="text-white bg-[#2c5172] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#2c5172] dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-toggle="EmailModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
+  
+  </div>
+
+    
 
   <router-link class="mx-3" to="/pwreset">비밀번호 재설정</router-link>
 </template>
--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -23,37 +23,49 @@
 
       <!-- 이름 입력 -->
       <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
-        <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이름</label>
+        <input type="text" name="floating_name" id="floating_name" class="block py-0.5 px-1 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">이름</label>
       </div>
 
       <!-- 이메일 입력 -->
-      <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
-        <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
+      <div class="flex items-center justify-between relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
+        <input type="email" name="floating_email" id="floating_email" class="block py-0.5 px-1 w-3/4 text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">이메일</label>
+        <button type="button" class="w-[76.4px] text-[#2c5172] dark:text-gray-300 text-sm border-2 border-[#2c5172] dark:border-gray-300 focus:outline-none rounded-md px-3 py-0.5 text-center">
+          <span>메일발송</span>
+        </button>
+      </div>
+
+      <!-- 인증번호 입력 -->
+      <div class="flex items-center justify-between relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
+        <input type="text" name="floating_verify" id="floating_verify" class="block py-0.5 px-1 w-3/4 text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_verify" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">인증번호</label>
+        <button type="button" class="w-[76.4px] text-[#2c5172] dark:text-gray-300 text-sm border-2 border-[#2c5172] dark:border-gray-300 focus:outline-none rounded-md px-3 py-0.5 text-center">
+          <span>확인</span>
+        </button>
       </div>
 
       <!-- 비밀번호 입력 -->
       <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group"> 
-        <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
+        <input type="password" name="floating_password" id="floating_password" class="block py-0.5 px-1 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">비밀번호</label>
       </div>
 
       <!-- 비밀번호 확인 -->
-      <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
-        <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호 확인</label>
+      <div class="relative z-0 mb-4 mr-auto ml-auto w-3/4 group">
+        <input type="password" name="floating_repeat_password" id="floating_repeat_password" class="block py-0.5 px-1 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">비밀번호 확인</label>
       </div>
 
       <!-- 분류  -->
-      <div class="flex justify-between items-center w-3/4 mr-auto ml-auto mb-6">
+      <div class="flex justify-between items-center w-3/4 mr-auto ml-auto mb-4">
         <div class="text-sm text-gray-500 dark:text-gray-400">분류</div>
         <div class="flex justify-between">
 
           <!-- 수강생 -->
           <div class="mr-4">
             <input type="radio" id="student" name="role" value="student" class="hidden peer" required checked>
-            <label for="student" class="flex items-center py-2 px-10 w-full text-gray-500 rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:text-gray-200 dark:peer-checked:bg-[#666666] peer-checked:border-[#2c5172] dark:peer-checked:border-gray-300 peer-checked:border-[3px] dark:peer-checked:border-[3px] peer-checked:text-[#2c5172] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-[#555555]">
+            <label for="student" class="flex items-center py-1 px-8 w-full text-gray-500 rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:text-gray-200 dark:peer-checked:bg-[#666666] peer-checked:border-[#2c5172] dark:peer-checked:border-gray-300 peer-checked:border-[3px] dark:peer-checked:border-[3px] peer-checked:text-[#2c5172] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-[#555555]">
               <div class="text-sm mr-auto ml-auto text-center font-semibold">수강생</div>
             </label>
           </div>
@@ -61,7 +73,7 @@
           <!-- 강의자 -->
           <div>
             <input type="radio" id="lecturer" name="role" value="lecturer" class="hidden peer">
-            <label for="lecturer" class="flex items-center py-2 px-10 w-full text-gray-500 rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:text-gray-200 dark:peer-checked:bg-[#666666] peer-checked:border-[#2c5172] dark:peer-checked:border-gray-300 peer-checked:border-[3px] dark:peer-checked:border-[3px] peer-checked:text-[#2c5172] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-[#555555]">
+            <label for="lecturer" class="flex items-center py-1 px-8 w-full text-gray-500 rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:text-gray-200 dark:peer-checked:bg-[#666666] peer-checked:border-[#2c5172] dark:peer-checked:border-gray-300 peer-checked:border-[3px] dark:peer-checked:border-[3px] peer-checked:text-[#2c5172] hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-[#555555]">
               <div class="text-sm mr-auto ml-auto text-center font-semibold">강의자</div>
             </label>
           </div>
@@ -70,36 +82,49 @@
 
       <!-- 웹캠 캡쳐 -->
       
-      <div class="flex justify-between items-center w-3/4 mr-auto ml-auto mb-3">
-        <div class="text-sm text-gray-500 dark:text-gray-400">사진</div>
+      <div class="flex justify-between items-center w-3/4 mr-auto ml-auto">
+        <div class="flex items-end">
+          <div class="text-sm mr-0.5 text-gray-500 dark:text-gray-400">사진</div>
+          <button data-tooltip-target="tooltip-right" data-tooltip-placement="right" type="button" class="text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-[#2c5172] dark:fill-gray-400" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+          </button>
+          <div id="tooltip-right" role="tooltip" class="inline-block absolute text-center invisible z-10 py-2 px-3 text-[12px] text-white dark:text-gray-200 bg-[#444444] rounded-lg tooltip dark:bg-[#222222]">
+            출석 인증과 자리 비움 여부를 판단하기 위해서<br/>
+            웹 카메라를 통한 얼굴 캡쳐가 필요합니다.
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        </div>
 
         <!-- 카메라 버튼 -->
-        <button v-if="!isCameraOpen" type="button" class="text-[#2c5172] dark:text-gray-300 text-sm border-2 border-[#2c5172] dark:border-gray-300 focus:outline-none rounded-md px-3 py-.5 text-center" @click="toggleCamera">
+        <button v-if="!isCameraOpen" type="button" class="text-[#2c5172] dark:text-gray-300 text-sm border-2 border-[#2c5172] dark:border-gray-300 focus:outline-none rounded-md px-3 py-0.5 text-center" @click="toggleCamera">
           <span>사진등록</span>
         </button>
 
-        <button v-if="isCameraOpen && !isLoading" type="button" :class="{'bg-[#fbb026] border-[#fbb026] font-semibold':isPhotoTaken && isCameraOpen,'bg-[#66bb6a] border-[#66bb6a]':!isPhotoTaken || !isCameraOpen}" class="text-white dark:text-gray-100 text-sm border-2 focus:outline-none rounded-md px-3 py-.5 text-center" @click="takePhoto">
+        <button v-if="isCameraOpen && !isLoading" type="button" :class="{'bg-[#fbb026] border-[#fbb026] font-semibold':isPhotoTaken && isCameraOpen,'bg-[#66bb6a] border-[#66bb6a]':!isPhotoTaken || !isCameraOpen}" class="text-white dark:text-gray-100 text-sm border-2 focus:outline-none rounded-md px-3 py-0.5 text-center" @click="takePhoto">
           <span v-if="isPhotoTaken && isCameraOpen">다시찍기</span>
           <span v-else>사진촬영</span>
         </button>
       </div>
 
       <!-- 카메라 로딩 -->
-      <div v-show="isCameraOpen && isLoading" class="flex justify-center items-center mr-auto ml-auto w-[384px] h-[288px] bg-black mb-6">
+      <div v-show="isCameraOpen && isLoading" class="flex justify-center items-center mr-auto ml-auto w-[384px] h-[288px] bg-black mt-2">
         <svg aria-hidden="true" role="status" class="w-[80px] h-[80px] text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#2c5172"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#92a8d1"/>
         </svg>
       </div>
       
-      <!-- 캠 화면 -->
-      <div v-if="isCameraOpen" v-show="!isLoading" class="mb-6" :class="{ 'opacity-0' : isShotPhoto }">
+      <!-- 사진등록을 위한 캠/사진 화면 -->
+      <div v-if="isCameraOpen" v-show="!isLoading" class="mt-2" :class="{ 'opacity-0' : isShotPhoto }">
         
         <div :class="{'opacity-0' : isShotPhoto}"></div>
-          
-        <video class="mr-auto ml-auto w-[384px] h-[288px]" v-show="!isPhotoTaken" ref="camera" autoplay></video>
-        
-        <canvas class="mr-auto ml-auto w-[384px] h-[288px]" v-show="isPhotoTaken" id="photoTaken" ref="canvas"></canvas>
+        <!-- 웹캠 -->
+        <video width="384" height="288" class="mr-auto ml-auto" v-show="!isPhotoTaken" ref="camera" autoplay></video>
+        <!-- 캡쳐 -->
+        <canvas width="384" height="288" class="mr-auto ml-auto" v-show="isPhotoTaken" id="photoTaken" ref="canvas"></canvas>
+
       </div>
       
       
@@ -110,7 +135,7 @@
         </a>
       </div> -->
     
-      <div class="text-center w-3/4 mr-auto ml-auto mb-6">
+      <div class="text-center w-3/4 mr-auto ml-auto mt-4 mb-6">
         <button type="submit" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">회원가입</button>
       </div>
     </form>
