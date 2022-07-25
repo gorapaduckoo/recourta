a385df2639740cec179164993ef03af68d37bf11
jiwan <jiwan1121@naver.com>
2022-07-25 16:21:22 +0900

    PWsearch_Modal_fixing

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -6,25 +6,27 @@
       <img class="ml-auto mr-auto" src="../assets/placeholder.png" alt="">
     </router-link>
     <form class="pt-10">
+      <!-- 이메일 입력 -->
       <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
-        <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
+        <input type="email" name="floating_email" id="floating_email" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
       </div>
-      <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group"> 
-        <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
+      <!-- 비밀번호 입력 -->
+      <div class="relative z-0 mb-3 mr-auto ml-auto w-3/4 group"> 
+        <input type="password" name="floating_password" id="floating_password" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
       </div>
-      <div class="flex w-3/4 mr-auto ml-auto mb-6">
+      <div class="flex items-center w-3/4 mr-auto ml-auto mb-6">
         <div class="flex items-center h-5">
           <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-[#ffffff] checked:bg-[#2c5172] dark:checked:bg-[#2c5172] focus:ring-[#ffffff] dark:focus:ring-[#444444] dark:focus:ring-offset-[#444444] rounded border border-gray-300 dark:border-[#888888] dark:bg-[#444444]">
         </div>
-        <label for="remember" class="ml-2 text-sm text-gray-900 dark:text-gray-400">이메일 저장</label>
+        <label for="remember" class="ml-2 text-xs text-gray-500 dark:text-gray-400">이메일 저장</label>
       </div>
-      <div class="text-center w-3/4 mr-auto ml-auto mb-6">
+      <div class="text-center w-3/4 mr-auto ml-auto mb-3">
         <button type="submit" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">로 그 인</button>
       </div>
     </form>
-    <div class="w-1/2 mr-auto ml-auto flex justify-center text-xs dark:text-gray-400">
+    <div class="w-1/2 mr-auto ml-auto mb-6 flex justify-center text-xs text-gray-500 dark:text-gray-400">
       <router-link class="w-20 text-right" to="/signup">회원가입</router-link>
       <div class="mx-4">|</div>
       <router-link class="w-20 text-left" to="/pwsearch">비밀번호 찾기</router-link>
--- frontend/src/views/PWsearchView.vue
+++ frontend/src/views/PWsearchView.vue
@@ -1,5 +1,6 @@
 <template>
   <DarkmodeButton />
+  <!-- <div modal-backdrop class="bg-[#444444] bg-opacity-50 fixed inset-0 z-40"></div> -->
   <div class="pt-10 w-1/3 min-w-[480px] ml-auto mr-auto border">
     <router-link to="/" class="absolute top-3">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
@@ -10,47 +11,49 @@
     <router-link to="/">
       <img class="ml-auto mr-auto" src="../assets/placeholder.png" alt="">
     </router-link>
+    <!-- 설명문장 -->
     <div class="w-3/4 ml-auto mr-auto dark:text-gray-300">
       <h1 class="mt-10 mb-6 font-bold">비밀번호 재설정</h1>
       <p class="text-sm">계정의 이메일을 입력해주세요.</p>
       <p class="text-sm">비밀번호 재설정 링크가 포함된 메일이 발송됩니다.</p>
     </div>
-    <form class="">
+    <!-- 이메일 입력 -->
+    <form id="email_input">
       <div class="mt-10 relative z-0 mb-10 mr-auto ml-auto w-3/4 group">
-        <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
-        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
+        <input type="email" name="floating_email" id="floating_email" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " required />
+        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
       </div>
-      <!-- modal button -->
-      <div class="mt-16 text-center w-3/4 mr-auto ml-auto mb-6">
-        <button data-modal-toggle="EmailModal" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">이메일 발송</button>
-      </div>  
-      <!-- Main modal -->
-      <div id="EmailModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden ml-auto mr-auto fixed z-50 w-1/2 md:inset-0 h-modal md:h-full">
-        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-          <!-- Modal content -->
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex justify-between items-start rounded-t dark:border-gray-600">
-              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="EmailModal">
-                  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                  <span class="sr-only">Close modal</span>
-              </button>
-            </div>
-            <!-- Modal body -->
-            <div class="py-4 text-center">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  비밀번호 재설정 메일을 보내시겠습니까?
-                </p>
-            </div>
-            <!-- Modal footer -->
-            <div class="flex items-center justify-center p-6 space-x-24 rounded-b border-t border-gray-200 dark:border-gray-600">
-                <button data-modal-toggle="EmailModal" type="button" class="text-white bg-[#2c5172] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#2c5172] dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-toggle="EmailModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
+    </form>
+    <!-- modal button -->
+    <div class="mt-16 text-center w-3/4 mr-auto ml-auto mb-6">
+      <button data-modal-toggle="EmailModal" class="text-white font-bold bg-[#2c5172] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">이메일 발송</button>
+    </div>  
+    <!-- Main modal -->
+    <div id="EmailModal" tabindex="-1" aria-hidden="true" class="hidden ml-auto mr-auto fixed z-50 w-1/3 min-w-[480px] inset-0 h-full h-modal">
+      <div class="relative p-5 w-full h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg dark:bg-[#444444]">
+          <!-- Modal header -->
+          <div class="flex justify-between items-start rounded-t dark:border-gray-600">
+            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="EmailModal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <div class="py-4 text-center">
+              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                비밀번호 재설정 메일을 보내시겠습니까?
+              </p>
+          </div>
+          <!-- Modal footer -->
+          <div class="flex items-center justify-center p-6 space-x-24 rounded-b border-t border-gray-200 dark:border-gray-600">
+              <button data-modal-toggle="EmailModal" type="button" class="text-white bg-[#2c5172] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#2c5172] dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+              <button data-modal-toggle="EmailModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
           </div>
         </div>
       </div>
-    </form>
+    </div>
   
   </div>
 
@@ -67,7 +70,10 @@ export default {
   components: {
     DarkmodeButton,
   },
+  
 }
+
+
 </script>
 
 <style>
