04908c72f7ffa04032143429f817565d4de91393
jiwan <jiwan1121@naver.com>
2022-07-31 01:53:56 +0900

    LoginView v-model ver 0.1

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,15 +5,15 @@
     <router-link to="/">
       <img class="ml-auto mr-auto" src="../assets/placeholder.png" alt="">
     </router-link>
-    <form @submit.prevent="loginCheck" class="pt-10">
+    <form @submit.prevent="loginSubmit" class="pt-10">
       <!-- 이메일 입력 -->
       <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
-        <input type="email" name="floating_email" ref="floating_email" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " />
+        <input type="input" name="floating_email" v-model.trim="floating_email" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " />
         <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
       </div>
       <!-- 비밀번호 입력 -->
       <div class="relative z-0 mb-3 mr-auto ml-auto w-3/4 group"> 
-        <input type="password" name="floating_password" ref="floating_password" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" "/>
+        <input type="password" name="floating_password" v-model.trim="floating_password" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" "/>
         <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
       </div>
       <div class="flex items-center w-3/4 mr-auto ml-auto mb-6 form-chec">
@@ -37,37 +37,52 @@
  
 </template>
 
-<script>
+<script setup>
 
 import DarkmodeButton from '../components/DarkmodeButton.vue'
+import { ref, reactive } from 'vue'
+import {useRouter} from 'vue-router'
 
-export default {
-  name: 'loginView',
-  components: {
-    DarkmodeButton,
-  },
-  setup() {
-    const loginCheck = () => {
-      console.log(this.$refs.floating_email.value)
-      const email = this.$refs.floating_email.value.trim();
-      const password = this.$refs.floating_password.value.trim();
+const route = useRouter()
 
-      let email_regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-      if(email_regex.test(email)) {
-        console.log('이메일이 맞습니다.')
-      } else {
-        console.log('이메일이 아닙니다.')
-      }
+let email_check=true
+let pw_check=true
+
+let floating_email = ref("")
+let floating_password = ref("")
+
+const test_id_pw = (id,pw) => {
+  if(id==="test@gmail.com"&&pw==="Test123@") return 1
+  return 0
+}
+
+const loginSubmit = () => {
       
-      
+      // 영문숫자로만이뤄진id(_-.이아이디포함x)@----.2-3글자(com)
+  let email_regex = new RegExp('[A-Za-z0-9._-]+@[a-z]+(\.[a-z]{2,3})+')
+  let pw_regex = new RegExp("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*?])(?=.{8,})")
+      // let pw_regex = new RegExp()
+    console.log(floating_email.value)
+    console.log(floating_password.value)
+    if(email_regex.test(floating_email.value)) {
+      email_check=true
+    } else {
+      console.log('옳바른 email을 입력하세요')
+      email_check=false
+    }
+    if(pw_regex.test(floating_password.value)) {
+      pw_check=true
+    } else {
+      console.log('옳바른 pw를 입력하세요.')
+      pw_check=false
+    }
+
+    if(email_check&&pw_check) {
+      if(test_id_pw(floating_email.value,floating_password.value)) route.replace({path:'/main'})
+      else console.log('일치하는 회원이 없습니다.')
     }
     
-    return {
-      loginCheck
-    }
-  },
-
-}
+  }
 </script>
 
 <style>
