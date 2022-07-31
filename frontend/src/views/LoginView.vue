773cb888de440571e832cff5bc8737c5ca1f61be
jiwan <jiwan1121@naver.com>
2022-07-31 17:00:13 +0900

    LoginView add label for wrong email pw

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -9,12 +9,14 @@
       <!-- 이메일 입력 -->
       <div class="relative z-0 mb-6 mr-auto ml-auto w-3/4 group">
         <input type="text" name="floating_email" v-model.trim="floating_email" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" " />
-        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
+        <label for="floating_email" :class="{'text-[#fe5358] dark:text-[#fe5358] peer-focus:text-[#fe5358] dark:peer-focus:text-[#fe5358]':!state.email_check,'text-gray-500 dark:text-gray-400 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6]':state.email_check,}" class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
+        <label v-if="!state.email_check" for="floating_email" class="absolute text-[4px] text-[#fe5358] dark:text-[#fe5358] -bottom-3.5 right-0">올바른 이메일을 입력하세요</label>
       </div>
       <!-- 비밀번호 입력 -->
       <div class="relative z-0 mb-3 mr-auto ml-auto w-3/4 group"> 
         <input type="password" name="floating_password" v-model.trim="floating_password" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-[#6c9cc6] focus:outline-none focus:ring-0 focus:border-[#2c5172] peer" placeholder=" "/>
-        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
+        <label for="floating_password"  :class="{'text-[#fe5358] dark:text-[#fe5358] peer-focus:text-[#fe5358] dark:peer-focus:text-[#fe5358]':!state.pw_check,'text-gray-500 dark:text-gray-400 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6]':state.pw_check,}" class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">비밀번호</label>
+        <label v-if="!state.pw_check" for="floating_password" class="absolute text-[4px] text-[#fe5358] dark:text-[#fe5358] -bottom-3.5 right-0">비밀번호는 8자 이상이고 영문, 숫자, 특수문자를 포함해야 합니다</label>
       </div>
       <div class="flex items-center w-3/4 mr-auto ml-auto mb-6 form-chec">
         <div class="flex items-center h-5 form-check">
@@ -45,8 +47,10 @@ import {useRouter} from 'vue-router'
 
 const route = useRouter()
 
-let email_check=true
-let pw_check=true
+const state = reactive({
+  email_check:true,
+  pw_check:true,
+})
 
 let floating_email = ref("")
 let floating_password = ref("")
@@ -65,19 +69,19 @@ const loginSubmit = () => {
     console.log(floating_email.value)
     console.log(floating_password.value)
     if(email_regex.test(floating_email.value)) {
-      email_check=true
+      state.email_check=true
     } else {
-      console.log('옳바른 email을 입력하세요')
-      email_check=false
+      console.log('올바른 email을 입력하세요')
+      state.email_check=false
     }
     if(pw_regex.test(floating_password.value)) {
-      pw_check=true
+      state.pw_check=true
     } else {
-      console.log('옳바른 pw를 입력하세요.')
-      pw_check=false
+      console.log('올바른 pw를 입력하세요.')
+      state.pw_check=false
     }
 
-    if(email_check&&pw_check) {
+    if(state.email_check&&state.pw_check) {
       if(test_id_pw(floating_email.value,floating_password.value)) route.replace({path:'/main'})
       else console.log('일치하는 회원이 없습니다.')
     }
