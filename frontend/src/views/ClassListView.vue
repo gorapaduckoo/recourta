7f71a4ee3de3a64c1957af301b6b6bfd75ea9bff
jiwan <jiwan1121@naver.com>
2022-08-01 09:59:13 +0900

    make temp page for ClassSettingView

--- frontend/src/views/ClassListView.vue
+++ frontend/src/views/ClassListView.vue
@@ -3,16 +3,16 @@
   <CustomNavbar />
   <div class="pt-[60px] container flex justify-around mx-auto">
     <div class="font-bold text-4xl">강의 목록</div>
-    <StudentCard />
-    <StudentCard />
-    <StudentCard />
+    <ListCard />
+    <ListCard />
+    <ListCard />
   </div>
 </template>
 
 <script setup>
 import DarkmodeButton from '../components/DarkmodeButton.vue'
 import CustomNavbar from '../components/CustomNavbar.vue'
-import StudentCard from '../components/StudentCard.vue'
+import ListCard from '../components/ListCard.vue'
 </script>
 
 <style>
---
+++ frontend/src/views/ClassSettingView.vue
@@ -0,0 +1,25 @@
+<template>
+  <DarkmodeButton />
+  <CustomNavbar />
+  
+</template>
+
+<script>
+import DarkmodeButton from '../components/DarkmodeButton.vue'
+import CustomNavbar from '../components/CustomNavbar.vue'
+
+export default {
+  
+  name: 'ClassSettingView',
+
+  components:{
+    DarkmodeButton,
+    CustomNavbar,
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
