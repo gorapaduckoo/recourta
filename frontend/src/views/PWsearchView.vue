3d388efb388e1a2d77f3377964e35b2eb07e6ce0
YoungJun <2753dudwns@naver.com>
2022-08-01 16:43:16 +0900

    Fixed Modal ver_0.8

--- frontend/src/views/PWsearchView.vue
+++ frontend/src/views/PWsearchView.vue
@@ -22,23 +22,23 @@
       <div class="mt-16 relative z-0 mb-10 mr-auto ml-auto w-3/4 group">
         <input type="text" v-model.trim="floating_email" id="floating_email" :class="{'border-[#fe5358] focus:border-[#fe5358] dark:border-[#fe5358] dark:focus:border-[#fe5358]':!state.email_check,'border-gray-300 focus:border-[#2c5172] dark:border-gray-600 dark:focus:border-[#6c9cc6]':state.email_check,}" class="block pt-2.5 pb-1 px-2 w-full text-sm bg-transparent border-0 border-b-2 appearance-none  focus:outline-none focus:ring-0 peer" placeholder=" " @click="onEmailClick"/>
         <label for="floating_email" :class="{'text-[#fe5358] dark:text-[#fe5358] peer-focus:text-[#fe5358] dark:peer-focus:text-[#fe5358]':!state.email_check,'text-gray-500 dark:text-gray-400 peer-focus:text-[#2c5172] dark:peer-focus:text-[#6c9cc6]':state.email_check,}" class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">이메일</label>
-        <label v-if="!state.email_check" for="floating_email" class="absolute text-[4px] text-[#fe5358] dark:text-[#fe5358] -bottom-3.5 right-0">올바른 이메일을 입력하세요</label>
+        <label v-if="!state.email_check" for="floating_email" class="absolute text-[4px] text-[#fe5358] dark:text-[#fe5358] -bottom-3.5 right-0">{{ state.msg }}</label>
       </div>
     </form>
 
     <!-- 이메일 발송 버튼 -->
     <div class="mt-10 text-center w-3/4 mr-auto ml-auto mb-6">
-      <button type="button" class="text-white font-bold bg-[#2c5172] hover:bg-[#325c81] dark:hover:bg-[#325c81] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]" data-bs-toggle="modal" data-bs-target="#emailSendModal">
+      <button form="email_input" type="submit" class="text-white font-bold bg-[#2c5172] hover:bg-[#325c81] dark:hover:bg-[#325c81] focus:outline-none rounded-lg w-full px-5 py-2.5 text-center dark:bg-[#2c5172]">
         이메일 발송
       </button>
     </div>
 
     <!-- Modal -->
-    <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="emailSendModal" tabindex="-1" aria-labelledby="emailSendModalLabel" aria-hidden="true">
+    <div class="modal fade bg-black bg-opacity-50 fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="emailSendModal" tabindex="-1" aria-hidden="true" @click.self="modalClose">
       <div class="modal-dialog modal-dialog-centered relative w-1/4 min-w-[360px] ml-auto mr-auto pointer-events-none">
         <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white dark:bg-[#4c4c4c] bg-clip-padding rounded-md outline-none text-current">
           <div class="modal-header flex flex-row-reverse flex-shrink-0 items-center justify-between px-4 pt-4 rounded-t-md">
-            <button type="button" data-bs-dismiss="modal" aria-label="Close">
+            <button type="button" data-bs-dismiss="modal" aria-label="Close" @click="modalClose">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-600 dark:text-neutral-300 hover:text-black dark:hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
@@ -46,12 +46,11 @@
           </div>
           <div class="modal-body py-4 text-center">
               <p class="font-medium">
-                비밀번호 재설정 메일을 보내시겠습니까?
+                성공적으로 메일이 발송되었습니다.
               </p>
             </div>
             <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 rounded-b-md space-x-3">
-              <button type="submit" form="email_input" class="text-white bg-[#2c5172] w-[62.3px] border border-[#2c5172] font-medium rounded-lg text-sm px-3 py-1.5 text-center hover:bg-[#325c81]" data-bs-dismiss="modal">예</button>
-              <button type="button" class="text-gray-500 w-[62.3px] rounded-lg border border-gray-200 text-sm font-medium px-3 py-1.5 dark:text-gray-300 dark:border-gray-500 hover:bg-gray-100 dark:hover:bg-[#555555]" data-bs-dismiss="modal">아니오</button>
+              <button type="button" class="text-gray-500 w-[62.3px] rounded-lg border border-gray-200 text-sm font-medium px-3 py-1.5 dark:text-gray-300 dark:border-gray-500 hover:bg-gray-100 dark:hover:bg-[#555555]" @click="modalClose">닫기</button>
             </div>
         </div>
       </div>
@@ -62,13 +61,14 @@
 
 <script setup>
 import DarkmodeButton from '../components/DarkmodeButton.vue'
-import {useRouter} from 'vue-router'
+import { useRouter } from 'vue-router'
 import { ref, reactive } from 'vue'
 
 const route = useRouter()
 
 const state = reactive({
-  email_check : true
+  email_check : true,
+  msg : ''
 })
 let floating_email = ref("")
 
@@ -87,18 +87,36 @@ const PWsearchSubmit = () => {
     state.email_check=true
   } else {
     console.log('올바른 email을 입력하세요')
+    state.msg='올바른 email을 입력하세요'
     state.email_check=false
   }
 
   if(state.email_check) {
-    if(test_email(floating_email.value)) route.replace({path:'/'})
-    else console.log('일치하는 회원이 없습니다.')
+    if(test_email(floating_email.value)) {
+      modalOpen()
+    }
+    else {
+      state.email_check=false
+      state.msg='일치하는 회원이 없습니다.'
+    }
   }
 }
 
 const onEmailClick = () => {
   state.email_check = true
 }
+
+const modalOpen = () => {
+  console.log('함수0')
+  document.getElementById('emailSendModal').classList.replace('hidden', 'show')
+}
+
+const modalClose = () => {
+  console.log('함수0')
+  document.getElementById('emailSendModal').classList.replace('show', 'hidden')
+  route.replace({path:'/'})
+}
+
 
 </script>
 
