940e2bf2f505ff4df2e885f00b97b7d06bbc273b
이지영 <jioung93@naver.com>
2022-08-03 21:14:33 +0900

    [S07P12A807-139] feat: 수강생 전체 알림 구현

---
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/notice/entity/Notice.java
@@ -0,0 +1,49 @@
+package com.ssafy.recourta.domain.notice.entity;
+
+import com.ssafy.recourta.domain.lecture.entity.Lecture;
+import com.ssafy.recourta.domain.user.entity.User;
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name="notice")
+public class Notice {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer noticeId;
+
+    @ManyToOne
+    @JoinColumn(name = "lectureId")
+    private Lecture lecture; // 송신 강의
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user; // 수신자
+
+    String title; // 제목
+    String content; // 내용
+    LocalDateTime datetime; // 송신일시
+
+    Boolean isChecked; // 1: 읽음, 0: 읽지않음
+
+    @Builder
+    public Notice(Lecture lecture, User user, String title, String content, LocalDateTime datetime, boolean isChecked) {
+        this.lecture = lecture;
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.datetime = datetime;
+        this.isChecked = isChecked;
+    }
+}
