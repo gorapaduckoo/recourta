d444befcbd069ab10c36777a1b6e0c5d4e656f72
YoungJun <2753dudwns@naver.com>
2022-08-03 10:25:08 +0900

    Fixed PastCard Page ver_0.5

--- frontend/src/components/PastCard.vue
+++ frontend/src/components/PastCard.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center mb-10">
     <div class="w-[400px] h-[500px] border-2 rounded-lg border-[#2c5172] dark:border-neutral-400 bg-[#fffbf8] dark:bg-[#4c4c4c]">
-      <a href="#!">
-        <img class="rounded-t-lg w-[400px] h-[225px]" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
-      </a>
+      <img class="rounded-t-lg w-[400px] h-[225px]" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
       <div class="p-6 flex flex-col justify-between h-[275px] dark:text-neutral-100">
         <div>
-          <div class="text-3xl font-bold dark:font-semibold mb-6 h-[72px]">데이터로 표현한 세상을 보고있는 우리와 나와 바다 놀러가</div>
+          <div class="text-3xl text-justify font-bold dark:font-semibold mb-6 h-[72px]">데이터로 표현한 세상을 보고있는 우리와 나와 바다 놀러가</div>
           <div class="text-xl font-semibold mt-4 text-end">김우석</div>
         </div>
         <div class="flex justify-end">
           <div class="text-lg text-start">
-            <p>목 10:00 ~ 12:00</p>
-            <p>금 09:00 ~ 12:00</p>
+            <p>시작 : 2022-09-01</p>
+            <p>종료 : 2022-10-11</p>
           </div>
         </div>
       </div>
