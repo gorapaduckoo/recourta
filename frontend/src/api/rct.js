8940fca179ea34d7c44a49ce764d85c03e953816
jiwan <jiwan1121@naver.com>
2022-08-04 15:19:50 +0900

    SignupView axios

--- frontend/src/api/rct.js
+++ frontend/src/api/rct.js
@@ -1,26 +1,26 @@
-const HOST = 'http://localhost:8081/recourta/'
+const HOST = 'http://localhost:8081/recourta'
 
-const USER = 'user/'
-const LECTURE = 'lecture/'
-const REGIST = 'registration/'
+const USER = '/user'
+const LECTURE = '/lecture'
+const REGIST = '/registration'
 
 export default {
     user: {
         signup: () => HOST + USER,
-        userauth: () => HOST + USER + 'auth/',
-        usercode: () => HOST + USER + 'code/',
-        userinfo: userid => HOST + USER + userid,
-        userimgmod: () => HOST + USER + 'img/',
-        userpwmod: () => HOST + USER + 'pw/',
+        userauth: () => HOST + '/auth/regist',
+        usercode: () => HOST + '/auth/code',
+        userinfo: userid => HOST + USER + '/' + userid,
+        userimgmod: () => HOST + USER + 'img',
+        userpwmod: () => HOST + USER + 'pw',
         
     },
     lecture: {
         lecturecreate: () => HOST,
-        lectureinfo: lectureid => HOST + LECTURE + lectureid,
+        lectureinfo: lectureid => HOST + LECTURE + '/' + lectureid,
     },
     regist: {
         regist: () => HOST + REGIST,
-        registinfo: regiid => HOST + REGIST + regiid, 
+        registinfo: regiid => HOST + REGIST + '/' + regiid, 
     },
     login: {
         login: () => HOST + 'login/',
