2207e016856b15fef7cb6ee8a3144223de6309e3
Yujiseul <seul_ll@naver.com>
2022-08-05 17:25:17 +0900

    feat: cors 에러 해결 구현

--- backend/Recourta/src/main/java/com/ssafy/recourta/global/config/SecurityConfig.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/global/config/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -37,6 +40,7 @@ public class SecurityConfig{
                 // h2-console 화면을 사용하기 위해 옵션을 disable
                 .and()
                     .logout()
+                    .logoutUrl("/logout")
                     .logoutSuccessUrl("http://localhost:5173/")
                 .and()
                     .httpBasic().disable()
@@ -57,4 +61,19 @@ public class SecurityConfig{
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
