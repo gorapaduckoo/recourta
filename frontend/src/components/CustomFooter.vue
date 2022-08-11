8e2c8365c81c96851e395762433c17328f3c3afe
YoungJun <2753dudwns@naver.com>
2022-08-11 17:25:02 +0900

    Fixed Logo

--- frontend/src/components/CustomFooter.vue
+++ frontend/src/components/CustomFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <div class="text-white fixed w-full bottom-0 text-center bg-[#92a8d1] z-30">footer</div>
+    <div class="text-xs text-white fixed w-full py-1 bottom-0 text-center bg-[#92a8d1] z-30">Copyright 2022, RECOURTA ALL RIGHTS RESERVED</div>
   </footer>
 </template>
 
