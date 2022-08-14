c90bbcd0a231abe457dc4419c0c1ff17302a926d
이지영 <jioung93@naver.com>
2022-08-15 04:26:01 +0900

    .

--- backend/Recourta/src/main/java/com/ssafy/recourta/domain/session/service/SessionServiceImpl.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/session/service/SessionServiceImpl.java
@@ -44,13 +44,15 @@ public class SessionServiceImpl implements SessionService{
             );
             LocalDate startDate;
 
-
             if (!isUpdate) { // 새로 생성된 강의인 경우 개강일부터 세션 생성
                 startDate = lecture.getStartDate();
             } else { // 강의정보가 업데이트된 경우 내일부터 세션 생성
                 startDate = LocalDate.now().plusDays(1);
             }
             LocalDate endDate = lecture.getEndDate();
+
+            System.out.println(startDate + " " + endDate);
+            System.out.println(s.getEndHour() + " " + s.getEndMinute());
             LocalDate targetDate = startDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.of(s.getWeekDay())));
             while(targetDate.isBefore(endDate.plusDays(1))){
                 Session session = Session.builder()
