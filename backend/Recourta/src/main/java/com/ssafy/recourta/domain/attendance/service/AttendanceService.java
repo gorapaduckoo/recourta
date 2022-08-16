cbea39bfb56db585d4a30f151e97279ee6645c0f
남궁준 <melon0soda@gmail.com>
2022-08-16 13:25:35 +0900

    fix

---
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/attendance/service/AttendanceService.java
@@ -0,0 +1,14 @@
+package com.ssafy.recourta.domain.attendance.service;
+
+import com.ssafy.recourta.domain.attendance.dto.response.AttendanceResponse;
+
+public interface AttendanceService {
+    // 해당 수업의 출석정보 자동 반영
+    void recordAllAttendanceOfSession(Integer sessionId);
+    // 해당 수업의 출석 여부 조회
+    AttendanceResponse.SessionAttendance getAttendanceOfSession(Integer sessionId);
+    // 해당 강의의 모든 출석 여부 조회
+    AttendanceResponse.LectureAttendance getAttendanceOfLecture(Integer lectureId);
+    // 특정 수업, 특정 수강생의 출석 여부 변경
+    void changeAttendance(Integer userId, Integer sessionId);
+}
--- backend/Recourta/src/main/java/com/ssafy/recourta/domain/checkinout/respository/CheckInOutRepository.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/checkinout/respository/CheckInOutRepository.java
@@ -4,7 +4,10 @@ import com.ssafy.recourta.domain.checkinout.entity.CheckInOut;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckInOutRepository extends JpaRepository<CheckInOut, Long> {
-
+    // 수업의 한 수강생의 모든 입퇴실 기록
+    List<CheckInOut> findAllByUserUserIdSessionSessionId(Integer userId, Integer sessionId);
 }
