b495def3b13901c459e85b4019e8b8fa81c6f348
Yujiseul <seul_ll@naver.com>
2022-08-17 18:07:14 +0900

    [S07P12A807-270] feat: 서버 경로 수정

--- backend/Recourta/src/main/java/com/ssafy/recourta/domain/user/service/MailService.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/user/service/MailService.java
@@ -89,7 +89,7 @@ public class MailService {
         msg+= "<p>감사합니다.<p>";
         msg+= "<br>";
         msg+= "<div align='center' style='border:1px solid black; font-family:verdana';><br>";
-        msg+= "<h3 style='color:blue;'><a href='http://127.0.0.1:5173/recourta/reset?code=" +
+        msg+= "<h3 style='color:blue;'><a href='https://recourta.ga/recourta/reset?code=" +
                 code+"'>[비밀번호 변경하기]</a></h3>";
         msg+= "<br/> ";
         msg+= "</div>";
