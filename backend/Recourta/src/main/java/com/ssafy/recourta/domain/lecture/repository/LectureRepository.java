93e5a41231c52c6d84b4801fa2f1b31c5af2b74e
이지영 <jioung93@naver.com>
2022-08-17 11:16:13 +0900

    [S07P12A807-256] feat: 수강신청 가능한 강의 조회 API에 타인이 개설한 강의 + 수강중이지 않은 강의 조건 추가

--- backend/Recourta/src/main/java/com/ssafy/recourta/domain/lecture/repository/LectureRepository.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/domain/lecture/repository/LectureRepository.java
@@ -26,6 +26,6 @@ public interface LectureRepository extends JpaRepository<Lecture, Integer> {
     // startDate >= 현재날짜 인 강의 찾기
     List<Lecture> findAllByUser_UserIdAndStartDateAfter(Integer userId, LocalDate yesterday);
 
-    // 개강 전인 강의 조회
-    List<Lecture> findAllByStartDateAfter(LocalDate date);
+    // 개강 전인 남의 강의 조회
+    List<Lecture> findAllByUser_UserIdNotAndStartDateAfter(Integer userId, LocalDate date);
 }
