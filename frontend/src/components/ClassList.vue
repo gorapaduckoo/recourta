82d876a1b5bd5614c1f1d09f7e43d2d4c3f23dee
jiwan <jiwan1121@naver.com>
2022-08-17 15:31:07 +0900

    ClassView reactive to mic

--- frontend/src/components/ClassList.vue
+++ frontend/src/components/ClassList.vue
@@ -5,7 +5,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
       </svg>
     </button>
-      <UserCam class="h-full max-h-[135px]" v-for="usr in props.userAll.slice(state.cnt*4, state.endIndex)"  @click="onclickuser(usr)" :key="usr.stream.connection.connectionId" :mainStreamManager="usr"/>    
+      <UserCam class="h-full max-h-[135px]" v-for="usr in props.userAll.slice(state.cnt*4, state.endIndex)" :isonMic="(props.onMic.indexOf(usr[4].stream.connection.connectionId)>=0)" :screenName="usr[1]" :isList="state.isList" @click="onclickuser(usr[4])" :key="usr[2]" :mainStreamManager="usr[4]"/>    
     <button @click="shiftright" class="hover:text-neutral-200 text-neutral-400">
       <svg class="h-8 w-8"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
@@ -22,12 +22,14 @@ const emit = defineEmits(["updateMainVideoStreamManager"])
 
 const state = reactive({
   cnt: 0,
-  endIndex : computed(() => calEndIndex())
+  endIndex : computed(() => calEndIndex()),
+  isList:true,
 })
 
 const props = defineProps({
   publisher: Object,
   userAll: Array,
+  onMic: Array,
 })
 
 const onclickuser = (user) => {
--- frontend/src/components/ClassMain.vue
+++ frontend/src/components/ClassMain.vue
@@ -1,13 +1,17 @@
 <template>
-  <UserCam class="max-w-[1050px] w-full lg:w-auto" :mainStreamManager="props.mainStreamManager"/>
+  <UserCam class="max-w-[1050px] w-full lg:w-auto" :isList="state.isList" :isonMic="false" :screenName="null" :mainStreamManager="props.mainStreamManager"/>
 </template>
 
 <script setup>
 import UserCam from "./UserCam.vue"
-import { defineProps } from 'vue'
+import { defineProps, reactive } from 'vue'
 
 const props = defineProps({
   mainStreamManager:Object,
+})
+
+const state = reactive({
+  isList : false,
 })
 
 </script>
