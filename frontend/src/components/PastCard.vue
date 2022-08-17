fcb92a78479042bb508f59c867b6ac69f9188eab
김영준 <2753dudwns@naver.com>
2022-08-18 04:18:30 +0900

    Past Class Page ver_1.0

--- frontend/src/components/PastCard.vue
+++ frontend/src/components/PastCard.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex justify-center mb-10">
     <div class="w-[400px] h-[500px] border-2 rounded-lg border-[#2c5172] dark:border-neutral-400 bg-[#fffbf8] dark:bg-[#4c4c4c]">
-      <img class="rounded-t-lg w-[400px] h-[225px]" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+      <img class="rounded-t-lg w-[400px] h-[225px]" :src="pastClassThumbnail" alt=""/>
       <div class="p-6 flex flex-col justify-between h-[275px] dark:text-neutral-100">
         <div>
-          <div class="text-3xl text-justify font-bold dark:font-semibold mb-6 h-[72px]">데이터로 표현한 세상을 보고있는 우리와 나와 바다 놀러가</div>
-          <div class="text-xl font-semibold mt-4 text-end">김우석</div>
+          <div class="text-3xl text-justify font-bold dark:font-semibold mb-6 h-[72px]">{{ pastClass.title }}</div>
+          <div class="text-xl font-semibold mt-4 text-end">{{ pastClass.teacher }}</div>
         </div>
         <div class="flex justify-end">
           <div class="text-lg text-start">
-            <p>시작 : 2022-09-01</p>
-            <p>종료 : 2022-10-11</p>
+            <p>시작 : {{ pastClass.startDate }}</p>
+            <p>종료 : {{ pastClass.endDate }}</p>
           </div>
         </div>
       </div>
@@ -18,10 +18,19 @@
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { useStore } from 'vuex'
 
-}
+const store = useStore()
+
+const props = defineProps({
+  pastClass : {
+    type: Object,
+  }
+})
+
+const pastClassThumbnail = store.state.lecture.lectureImgFirstUrl+props.pastClass.lectureImg
+
 </script>
 
 <style>
