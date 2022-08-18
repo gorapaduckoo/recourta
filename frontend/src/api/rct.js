9a85a39c4ceb40b991f2f1db126e516cfcfe2a68
남궁준 <melon0soda@gmail.com>
2022-08-18 21:26:05 +0900

    Update frontend/src/api/rct.js

--- frontend/src/api/rct.js
+++ frontend/src/api/rct.js
@@ -1,4 +1,5 @@
 const HOST = 'https://recourta.ga'
+const HOST_BACK = 'https://recourta.ga/recourta'
 
 const USER = '/user'
 const LECTURE = '/lecture'
@@ -10,39 +11,39 @@ const openvidu_server_secret = "MY_SECRET"
 
 export default {
   user: {
-    signup: () => HOST + USER,
-    userauth: () => HOST + '/auth/regist',
-    usercode: () => HOST + '/auth/code',
-    tokenrefresh: () => HOST + '/auth/refresh',
-    userinfo: userid => HOST + USER + '/' + userid,
-    userimgmod: () => HOST + USER + '/img',
-    userpwmod: () => HOST + USER + '/pw',
+    signup: () => HOST_BACK + USER,
+    userauth: () => HOST_BACK + '/auth/regist',
+    usercode: () => HOST_BACK + '/auth/code',
+    tokenrefresh: () => HOST_BACK + '/auth/refresh',
+    userinfo: userid => HOST_BACK + USER + '/' + userid,
+    userimgmod: () => HOST_BACK + USER + '/img',
+    userpwmod: () => HOST_BACK + USER + '/pw',
       
   },
   lecture: {
-    lecturecreate: () => HOST + LECTURE,
-    lectureinfo: lectureid => HOST + LECTURE + '/' + lectureid,
-    availablelist: userid => HOST + LECTURE + '/' + userid + '/availableList',
-    currentteaching: userid => HOST + LECTURE + '/' + userid + '/currentTeachingList',
-    currentlearning: userid => HOST + LECTURE + '/' + userid + '/currentLectureList',
+    lecturecreate: () => HOST_BACK + LECTURE,
+    lectureinfo: lectureid => HOST_BACK + LECTURE + '/' + lectureid,
+    availablelist: userid => HOST_BACK + LECTURE + '/' + userid + '/availableList',
+    currentteaching: userid => HOST_BACK + LECTURE + '/' + userid + '/currentTeachingList',
+    currentlearning: userid => HOST_BACK + LECTURE + '/' + userid + '/currentLectureList',
   },
   regist: {
-    regist: () => HOST + REGIST,
-    registinfo: regiid => HOST + REGIST + '/' + regiid,
-    registlearning: userid => HOST + REGIST + USER + '/current/' + userid,
-    registlearned: userid => HOST + REGIST + USER + '/previous/' + userid,
-    currentstudentlist: lectureid => HOST + REGIST + '/lecture/' + lectureid,
+    regist: () => HOST_BACK + REGIST,
+    registinfo: regiid => HOST_BACK + REGIST + '/' + regiid,
+    registlearning: userid => HOST_BACK + REGIST + USER + '/current/' + userid,
+    registlearned: userid => HOST_BACK + REGIST + USER + '/previous/' + userid,
+    currentstudentlist: lectureid => HOST_BACK + REGIST + '/lecture/' + lectureid,
   },
   login: {
-    login: () => HOST + '/login',
-    pwsearch: () => HOST + '/reset',
-    pwreset: () => HOST + '/reset/pw',
-    logout: () => HOST + '/logout',
+    login: () => HOST_BACK + '/login',
+    pwsearch: () => HOST_BACK + '/reset',
+    pwreset: () => HOST_BACK + '/reset/pw',
+    logout: () => HOST_BACK + '/logout',
   },
   webrtc: {
     openvd_url:() => "https://"+location.hostname+openvidu_port,
     openvd_secret:() => openvidu_server_secret,
-    checkin:() => HOST + CHECK + "/in",
-    checkout:() => HOST + CHECK + "/out",
+    checkin:() => HOST_BACK + CHECK + "/in",
+    checkout:() => HOST_BACK + CHECK + "/out",
   }
 }
