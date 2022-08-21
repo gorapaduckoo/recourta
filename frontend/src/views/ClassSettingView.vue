530462c66b85c9b741ea82ed324b2b41eb5d037b
이지완 <jiwan1121@naver.com>
2022-08-21 20:20:45 +0900

    fix rout to store

--- frontend/src/views/ClassSettingView.vue
+++ frontend/src/views/ClassSettingView.vue
@@ -158,7 +158,7 @@ const state = reactive({
 // 강의 폐쇄 함수
 const deleteLecture = async () => {
   await axios({
-    url: rct.lecture.lectureinfo(Number(rout.params.lecId)),
+    url: rct.lecture.lectureinfo(store.state.lecture.lectureId),
     method: 'delete',
     headers: {
       Authorization: store.state.user.accessToken,
