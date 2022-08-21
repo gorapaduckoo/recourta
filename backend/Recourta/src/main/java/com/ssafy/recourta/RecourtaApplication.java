1dc2daacb54cd7bf43388727c176f15d93101273
남궁준 <melon0soda@gmail.com>
2022-08-21 17:04:58 +0900

    Update backend/Recourta/src/main/java/com/ssafy/recourta/RecourtaApplication.java

--- backend/Recourta/src/main/java/com/ssafy/recourta/RecourtaApplication.java
+++ backend/Recourta/src/main/java/com/ssafy/recourta/RecourtaApplication.java
@@ -2,17 +2,10 @@ package com.ssafy.recourta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class RecourtaApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecourtaApplication.class, args);
     }
-
-    @Bean
-	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
-		return new HiddenHttpMethodFilter();
-	}
 }
